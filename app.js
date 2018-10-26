194be7f72f1e22bae5aa98e38aee757ad357a14b
Gulia, Vikram <vgulia@hawk.iit.edu>
2018-10-26 16:25:08 -0600

    updated a few things

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ module.exports = function (db) {
 
     // view engine setup
     app.set('views', path.join(__dirname, 'views'));
-    app.set('view engine', 'jade');
+    app.set('view engine', 'pug');
 
     // uncomment after placing your favicon in /public
     //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -27,6 +27,10 @@ module.exports = function (db) {
     app.use('/', index);
     app.use('/users', users);
     app.use('/cars', cars);
+
+    app.get('*', (req, res) => {
+        res.redirect('/');
+    });
 
     // catch 404 and forward to error handler
     app.use(function (req, res, next) {
